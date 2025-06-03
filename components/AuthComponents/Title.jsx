6d1c5fd5ab13eb9565caeeaa777e19070ed7aa1c
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-03 17:31:22 +0300

    update

---
+++ components/AuthComponents/Title.jsx
@@ -0,0 +1,26 @@
+import { Text } from "react-native";
+
+export default function Title({
+  titleText,
+  color,
+  marginTop,
+  marginBottom,
+  FontSize,
+  FontWeight,
+}) {
+  return (
+    <>
+      <Text
+        style={{
+          color: color,
+          marginBottom: marginBottom,
+          marginTop: marginTop,
+          fontSize: FontSize,
+          fontWeight:FontWeight
+        }}
+      >
+        {titleText}
+      </Text>
+    </>
+  );
+}
