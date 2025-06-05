160c9f1650b317d8ff8cf78358709662ae1d123e
Amrr-Maherr <amrr.maherr242gmail.com>
2025-06-05 14:18:26 +0300

    update

--- components/ProductComponent/ProductCard.jsx
+++ components/ProductComponent/ProductCard.jsx
@@ -1,39 +1,52 @@
 import { Image, Text, View } from "react-native";
 import Title from "../UiComponents/Title";
 
-export default function ProductCard() {
+export default function ProductCard({product}) {
     return (
       <>
         <View
           style={{
             height: "auto",
             width: 173,
-            height:248,
+            height: 248,
             borderRadius: 20,
             padding: 15,
             alignItems: "center",
             justifyContent: "space-between",
             borderColor: "#7C7C7C",
             borderWidth: 1,
-            margin:10
+            margin: 10,
           }}
         >
           <View style={{ width: 100, height: 100 }}>
             <Image
-              source={require("../../assets/product_image.png")}
+              source={{ uri: product.imageCover }}
               style={{ width: "100%", height: "100%" }}
             />
           </View>
-          <View>
-            <Title titleText="Exclusive Offer" />
-            <Title titleText="See All" />
+          <View
+            style={{
+              alignItems: "flex-start",
+              width: "100%",
+              marginHorizontal: 17,
+            }}
+          >
+            <Title titleText={product.title.slice(0, 10)} />
+            <Title titleText={product.brand.name} FontSize={14} color="#7C7C7C" />
           </View>
-          <View style={{ alignItems: "center", justifyContent: "space-between",width:"100%",flexDirection:"row-reverse" }}>
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              flexDirection: "row-reverse",
+            }}
+          >
             <Image
               style={{ width: 54, height: 54 }}
               source={require("../../assets/Group 6813.png")}
             />
-            <Title titleText="44.4$" />
+            <Title titleText={product.price + "$"} />
           </View>
         </View>
       </>
--- components/ProductComponent/ProductSection.jsx
+++ components/ProductComponent/ProductSection.jsx
@@ -1,10 +1,28 @@
 import { ScrollView, View } from "react-native";
 import ProductCard from "./ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function ProductSection({direction}) {
+export default function ProductSection({ direction }) {
+    const [data,setData] = useState([])
+    const GetData = async () => {
+        try {
+            let response = await axios.get("https://ecommerce.routemisr.com/api/v1/products")
+        setData(response.data.data.slice(0,10))
+        console.log(response.data.data); 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        GetData()
+    },[])
     return (
       <>
-        <ScrollView showsHorizontalScrollIndicator={false} horizontal={direction} >
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal={direction}
+        >
           <View
             style={{
               alignItems: "center",
@@ -13,14 +31,9 @@ export default function ProductSection({direction}) {
               flexDirection: "row",
             }}
           >
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {data.map((product) => (
+                <ProductCard product={product} key={product.id} />
+            ))}
           </View>
         </ScrollView>
       </>
